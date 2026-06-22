19144274b3c059e6e7cc9304b5d71856f58958af
sebnunezo <seb.nunezo@duocuc.cl>
2026-06-21 23:42:37 -0400

    Refactor decimas.py to add menu and exit functionality
    parte agregada sebastian

--- decimas.py
+++ decimas.py
@@ -58,3 +58,40 @@ def registrar_reserva(lista):
         print(f"Listo! Total: ${total}, Categoria: {categoria}")
     except ValueError:
         print("ERROR: ingrese solo numeros donde corresponde ")
+
+#PARTE SEBA
+def salir_sistema():
+    print("Gracias por usar el sistema, Saliendo....")
+    return True
+
+def menu():
+    while True:
+        limpiar_pantalla()
+        print("1. Registrar reserva")
+        print("2. Buscar reserva")
+        print("3. Actualizar reserva")
+        print("4. Eliminar reserva")
+        print("5. Mostrar reservas")
+        print("6. Mostrar estadísticas")
+        print("7. Salir")
+
+        opcion = input("Ingrese una Opcion: ")
+
+        if opcion == "1":
+            registrar_reserva(reservas)
+        elif opcion == "2":
+            buscar_reserva_menu(reservas)
+        elif opcion == "3":
+            actualizar_reserva(reservas)
+        elif opcion == "4":
+            eliminar_reserva(reservas)
+        elif opcion == "5":
+            mostrar_reservas(reservas)
+        elif opcion == "6":
+            mostrar_estadisticas(reservas)
+        elif opcion == "7":
+            if salir_sistema():
+                break
+        else: 
+            print("Invalido")
+menu()
